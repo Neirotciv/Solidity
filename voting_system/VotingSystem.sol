0ae6ddf407270b34b6ef0835f5d0b29bbd772516
VicLam <tanacetifolia@gmail.com>
2022-06-11 12:13:36 +0200

    voting system update

--- voting_system/VotingSystem.sol
+++ voting_system/VotingSystem.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.14;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
+/*
+    Le vote n'est pas secret
+    Chaque électeur peux voir le code des autres
+    Le gagnant est déterminé à la majorité
+    La proposition qui obtient le plus de voix l'emporte
+*/
+
 contract Voting is Ownable {
     address admin;
     uint winningProposalId;
@@ -46,40 +53,84 @@ contract Voting is Ownable {
         session = _status;
     }
 
-    // Add voters to the whitelist
+    /*  
+        L'administrateur du vote enregistre une liste blanche d'électeurs 
+        identifiés par leur adresse Ethereum.
+    */
     function addingVotersToWhitelist(address _address) external onlyOwner {
         voters[_address] = Voter(true, false, 0);
     }
 
-    // Start new session
+    // L'administrateur du vote peut commencer une session d'enregistrement de la proposition.
     function startPropositionSession() public onlyOwner {
         emit WorkflowStatusChange(session, WorkflowStatus.ProposalsRegistrationStarted);
         session = WorkflowStatus.ProposalsRegistrationStarted;
     }
 
-    /*  Les électeurs inscrits sont autorisés à enregistrer 
+    // L'administrateur du vote commence la session de vote
+    function startVotingSession() public onlyOwner {
+        emit WorkflowStatusChange(session, WorkflowStatus.VotingSessionStarted);
+        session = WorkflowStatus.VotingSessionStarted;
+    }
+
+    /*  
+        Les électeurs inscrits sont autorisés à enregistrer 
         leurs propositions pendant que la session d'enregistrement 
         est active.
     */
     function addProposal(string memory _description) external {
-        require(session == WorkflowStatus.ProposalsRegistrationStarted, "The session has not started yet");
         require(voters[msg.sender].isRegistered, "You are not registered");
+        require(session == WorkflowStatus.ProposalsRegistrationStarted, "The session has not started yet");
         proposalList.push(Proposal(_description, 0));
         voters[msg.sender].hasVoted = true;
     }
 
+    // Mettre fin à une session, proposition et vote
     function endCurrentSession() external onlyOwner {
         require(
             session == WorkflowStatus.ProposalsRegistrationStarted ||
             session == WorkflowStatus.VotingSessionStarted,
             "No session to end"
         );
+        
+        WorkflowStatus previousStatus = session;
         if (session == WorkflowStatus.ProposalsRegistrationStarted) {
             session = WorkflowStatus.ProposalsRegistrationEnded;
-            emit WorkflowStatusChange(session, WorkflowStatus.ProposalsRegistrationStarted);
+            emit WorkflowStatusChange(previousStatus, session);
         } else {
             session = WorkflowStatus.VotingSessionEnded;
-            emit WorkflowStatusChange(session, WorkflowStatus.ProposalsRegistrationStarted);
+            emit WorkflowStatusChange(previousStatus, session);
         }
+    }
+
+    // Les électeurs inscrits votent pour leur proposition préférée.
+    function voteForProposition(uint _proposalId) public {
+        // Doit être inscrit et n'a pas encore voté
+        require(voters[msg.sender].isRegistered && !voters[msg.sender].hasVoted, "You are not registered");
+        require(session == WorkflowStatus.VotingSessionStarted, "The voting session has not started yet");
+        proposalList[_proposalId].voteCount++;
+        voters[msg.sender].hasVoted = true;
+    }
+
+    // Comptabiliser les votes
+    function countTheVotes() public onlyOwner {
+        uint id;
+        uint count;
+        // Parcourir la liste des propositions
+        for (uint i; i < proposalList.length; i++) {
+            if (proposalList[i].voteCount > count) {
+                count = proposalList[i].voteCount;
+                id = i;
+            }
+        }
+        winningProposalId = id;
+    }
+
+    function getAllProposals() public view returns (Proposal[] memory) {
+        return proposalList;
+    }
+
+    function getWinningProposal() public view  returns (Proposal memory) {
+        return proposalList[winningProposalId];
     }
 }
